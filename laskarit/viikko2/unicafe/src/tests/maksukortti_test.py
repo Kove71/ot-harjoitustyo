157c8b94cd8e9e1608d2b47215ae03534d4d7ba9
Vesterinen Kosti I <kostives@cubbli20-gold.local>
2021-03-30 18:30:47 +0300

    .gitignore päivitetty

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,25 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_alustettu_raha_on_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_arvon_lataus(self):
+        self.maksukortti.lataa_rahaa(90)
+        self.assertEqual(str(self.maksukortti), "saldo: 1.0")
+    
+    def test_saldo_vähenee(self):
+        self.maksukortti.ota_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.0")
+    
+    def test_saldo_ei_muutu_jos_nostaa_liikaa(self):
+        self.maksukortti.ota_rahaa(20)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_ota_rahaa_palauttaa_true_jos_onnistui(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+
+
+    def test_ota_rahaa_palauttaa_false_jos_ei_onnistu(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(20), False)
