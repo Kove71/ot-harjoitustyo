f123d4c80bf0f8755ad089433a09c227cd016e0a
Vesterinen Kosti I <kostives@cubbli20-gold.local>
2021-04-13 16:35:25 +0300

    coverage and testing expanded. added .coveragerc so that the html file doesn't get spammed

--- src/tests/imdb_request_test.py
+++ src/tests/imdb_request_test.py
@@ -1,1 +1,14 @@
 import unittest
+from imdb_search import IMDBSearch
+
+class TestIMDBSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.search = IMDBSearch()
+
+    def test_search_with_searchword(self):
+        self.assertEqual(self.search.request_search("up"), True)
+    
+    def test_search_without_searchword(self):
+        self.assertEqual(self.search.request_search(""), False)
+
