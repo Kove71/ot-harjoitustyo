ab3a51c27cbb3a5e348bb10527ea9a1afe3fc0ca
meitsin-ohte <81042269+meitsin-ohte@users.noreply.github.com>
2021-04-13 21:01:54 +0300

    Update imdb_search.py

--- src/imdb_search.py
+++ src/imdb_search.py
@@ -2,11 +2,20 @@ import requests
 from search_movie import SearchMovie
 from database_test import DatabaseConnection
 
+# Luokka on vastuussa api-kutsuista. Tällä hetkellä ainoa api-kutsu on imdb-apin SearchMovie, joka palauttaa sopivat elokuvat.
+# Sovelluslogiikkaassa on tällä hetkellä sekaisin myös käyttöliittymän toiminnallisuutta. Ne lähtevät asap pois, kunhan saan guin käyntiin.
+#
+
 class IMDBSearch:
 
     def __init__(self):
         self.url = "https://imdb-api.com/en/API/SearchMovie/k_grjsr7l7/"
+    
+    # Metodi request_search() saa parametriksi hakusanan, jonka perusteella tehdään api-kutsu. Se dekoodataan käyttämällä Response.json(),
+    # jolloin siitä tulee dict. SearchMovie on elokuvaluokka, jolla on konstruktoreina elokuvan tietoja. Näistä muodostetaan uusi lista self.movie_list
+    # Jos kaikki onnistui hyvin, palautetaan totuusarvo True ja False jos jossain meni jokin vialle.
 
+    
     def request_search(self, searchword: str):
         if len(searchword) == 0:
             return False
@@ -23,6 +32,9 @@ class IMDBSearch:
         else:
             return False
     
+    #Osittain käyttöliittymämetodi, joka on vastuussa oikean elokuvan valitsemisesta. Elokuva valitaan käyttämällä listan movie_list indeksejä
+    #Jos elokuva valitaan, kutsutaan metodia add_movie_to_database
+    
     def select_movie(self):
         for i in self.movie_list:
             print(i.index, i.title, i.release_date)
@@ -36,6 +48,8 @@ class IMDBSearch:
             print("Movie added: ")
             print(self.movie_list[movie_selection - 1].title, self.movie_list[movie_selection - 1].release_date)
             self.add_movie_to_database(self.movie_list[movie_selection - 1])
+    
+    #Lisää valitun elokuvan tietokantaan.
     
     def add_movie_to_database(self, selected_movie):
         connection = DatabaseConnection()
