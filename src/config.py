fb9c48846216180ac90b5ec8ff88476066b0a5a8
meitsin-ohte <81042269+meitsin-ohte@users.noreply.github.com>
2021-04-13 21:07:14 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 import os
 
+# Sisältää kovakoodatun polun tietokantaan. Omaa testaus- ja koodauskäyttöä varten tarkistaa jos nykyinen working dir on src-hakemistossa
+
 if os.environ["PWD"] == "/home/kostives/ot-harjoitustyo/src":
     database_file_path = "../data/database.db"
 else:
